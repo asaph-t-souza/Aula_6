a06765d0c76c25bcad6cce1158bbefc521efa1c5
asaph-t-Souza <asaph.net@gmail.com>
2023-01-16 20:18:52 -0300

    Aula-6-7

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ var saida = document.getElementById("saida");
 var imprimir = "";
 
 var semana = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"];
+alert(semana.push("Oitava", "Sabado 2","Domingo 2"));
+alert(semana.pop());
+semana.shift();
+
+alert(semana.indexOf("Quinta"));
 
 for(var i = 0; i < semana.length; i++){
     imprimir += semana[i] + "<br>";
